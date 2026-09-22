0f20a21502e7e26bb4e7666c97c4fdf6a2bc12fa
courtneydragota <cosb280@mail.ubc.ca>
2026-09-22 10:14:38 -0700

    some message

--- Analyses/Mtcars analysis.R
+++ Analyses/Mtcars analysis.R
@@ -5,3 +5,11 @@ head(mtcars)
 mean_mpg <- mean(mtcars$mpg)
 
 print(mean_mpg)
+
+
+#Stage changes(write these in the terminal)
+  #git add .
+#Commit changes 
+  #git commit -m "some message"
+#Push changes 
+  #git push origin main
